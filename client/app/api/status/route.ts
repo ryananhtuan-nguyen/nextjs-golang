945ecb02126ccf9687287c142989da466466ffdb
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-31 12:44:30 +1300

    handling error

--- client/app/api/status/route.ts
+++ client/app/api/status/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const response = await fetch('http://localhost:4000/status');
-  const text = await response.json();
-  return NextResponse.json(text, { status: 200 });
+  try {
+    const response = await fetch('http://localhost:4000/status');
+    if (!response.ok) {
+      throw new Error('Error fetching data');
+    }
+    const text = await response.json();
+    return NextResponse.json(text, { status: 200 });
+  } catch (error) {
+    console.log('ERROR =>>>>', error);
+    return new NextResponse('Failed fetching data', { status: 500 });
+  }
 }
