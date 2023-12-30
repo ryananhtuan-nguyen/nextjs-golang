284d7166a8d52bbfdf17e4c4da73f1739b722a94
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-31 12:54:08 +1300

    setting no cache for serverstatus

--- client/components/server-status.tsx
+++ client/components/server-status.tsx
@@ -12,23 +12,34 @@ const ServerStatus = () => {
   const [status, setStatus] = useState<ServerStatus | null>(null);
   const fetchStatus = async () => {
     try {
-      const response = await axios.get('/api/status');
+      const response = await axios.get('/api/status', {
+        headers: {
+          'Cache-Control': 'no-cache',
+        },
+      });
+      setStatus(response.data);
     } catch (error) {
+      setStatus(null);
       if (error instanceof Error) console.log(error.message);
     }
-    // setStatus(response.data);
   };
 
   useEffect(() => {
     fetchStatus();
-  }, []);
+  }, [status]);
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       <ul className="flex flex-col gap-4">
         <span className="font-bold mb-10">ServerStatus:</span>
-        <li>Status: {status?.status}</li>
-        <li>Environment: {status?.environment}</li>
-        <li>Version: {status?.version}</li>
+        {status !== null ? (
+          <>
+            <li>Status: {status.status}</li>
+            <li>Environment: {status.environment}</li>
+            <li>Version: {status.version}</li>
+          </>
+        ) : (
+          <span className="font-semibold">Server not available</span>
+        )}
       </ul>
     </div>
   );
