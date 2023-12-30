9d251570781160cc67af7b8d6954d106cad08b03
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-31 12:32:05 +1300

    fetching data

---
+++ client/components/server-status.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type ServerStatus = {
+  status: string;
+  environment: string;
+  version: string;
+};
+
+const ServerStatus = () => {
+  const [status, setStatus] = useState<ServerStatus | null>(null);
+  const fetchStatus = async () => {
+    const response = await axios.get('/api/status');
+    setStatus(response.data);
+  };
+
+  useEffect(() => {
+    fetchStatus();
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-center mt-10">
+      <ul className="flex flex-col gap-4">
+        <span className="font-bold mb-10">ServerStatus:</span>
+        <li>Status: {status?.status}</li>
+        <li>Environment: {status?.environment}</li>
+        <li>Version: {status?.version}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ServerStatus;
