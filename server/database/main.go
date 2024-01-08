a51e0fabf8a723837cdc895131788e5fe79c9d64
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-08 20:25:33 +1300

    feat: initial table

--- server/database/main.go
+++ server/database/main.go
@@ -16,11 +16,53 @@ func main() {
 	}
 
 	//create user table
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, nickname TEXT, password TEXT, )")
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users 
+		(
+			id TEXT PRIMARY KEY, 
+			nickname TEXT, 
+			password TEXT
+		)
+	`)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS wardrobe (id INTEGER PRIMARY KEY)")
+	//wardrobe table
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS wardrobe (
+			id TEXT PRIMARY KEY,
+			user_id TEXT NOT NULL REFERENCES users(id),
+			name TEXT,
+			description TEXT,
+			category TEXT,
+			image TEXT NOT NULL,
+			public_id TEXT
+		);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//outfit table
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS outfits (
+        id TEXT PRIMARY KEY,
+        user_id TEXT NOT NULL REFERENCES users(id),
+        img TEXT NOT NULL,
+        description TEXT,
+        top_id INTEGER REFERENCES wardrobe(id),
+        bottom_id INTEGER REFERENCES wardrobe(id),
+        outer_id INTEGER REFERENCES wardrobe(id),
+        accessories_id INTEGER REFERENCES wardrobe(id),
+        footwear_id INTEGER REFERENCES wardrobe(id),
+        date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        public_id TEXT
+    );
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
